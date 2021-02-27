f7331dc2c5c69bdad559362da14cf56bf426da6c
CertaNet <certanet@users.noreply.github.com>
2021-02-27 21:58:18 +0000

    Updated min Python version

--- setup.py
+++ setup.py
@@ -23,8 +23,6 @@ setuptools.setup(
         'Intended Audience :: Developers',
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
